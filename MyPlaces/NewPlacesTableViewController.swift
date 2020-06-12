989ff366bb810031fd5fdf2700a649490392fd38
AlexeyAnanin <ananin.a.a@mail.ru>
2020-06-12 18:49:40 +0300

    image Of Place Action Sheet Customization

--- MyPlaces/NewPlacesTableViewController.swift
+++ MyPlaces/NewPlacesTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewPlacesTableViewController: UITableViewController {
     
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,8 +21,32 @@ class NewPlacesTableViewController: UITableViewController {
     // MARK: - Table View Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cameraIcon = #imageLiteral(resourceName: "camera")
+        let photoGalleryIcon = #imageLiteral(resourceName: "photo")
+        
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.choseImagePicker(source: .camera)
+            }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let photoGallery = UIAlertAction(title: "Photo Gallery", style: .default) { _ in
+                self.choseImagePicker(source: .photoLibrary)
+            }
+            photoGallery.setValue(photoGalleryIcon, forKey: "image")
+            photoGallery.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photoGallery)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
         }
@@ -36,6 +62,27 @@ extension NewPlacesTableViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+}
+
+// MARK: - Work with image
+
+extension NewPlacesTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    func choseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true // Allows you to edit the selected image
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFit
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
+    }
 }
